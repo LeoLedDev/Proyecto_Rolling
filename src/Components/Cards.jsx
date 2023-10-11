02ca211de3b2d4b103355422ba36a26b00093e39
Giuliana Ricci <rigiu25@gmail.com>
2023-10-11 12:58:08 -0300

    se crean cards

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React from 'react';
+import CardComp from './CardComp';
+import CardServ from './CardServ';
 
 const Cards = () => {
   return (
-    <div>Cards</div>
+    <div className="container">
+      <div className="row">
+        <div className='col-sm-12 col-md-6 col-lg-4 col-xl-3'>
+          <CardComp />
+          <hr />
+          
+          {/* <CardServ/> */}
+        </div>
+        <div className='col-sm-12 col-md-6 col-lg-4 col-xl-3'>
+          <CardComp />
+          <hr />
+          {/* <CardServ/> */}
+        </div>
+        <div className='col-sm-12 col-md-6 col-lg-4 col-xl-3'>
+          <CardComp />
+          <hr />
+          {/* <CardServ/> */}
+        </div>
+        <div className='col-sm-12 col-md-6 col-lg-4 col-xl-3'>
+          <CardComp />
+          <hr />
+          {/* <CardServ/> */}
+        </div>
+      </div>
+    </div>
   )
 }
 
